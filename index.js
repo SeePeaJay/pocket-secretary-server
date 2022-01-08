5f243c84a10214819fb741e4e3c4ce0a39693dbf
SeePeaJay <seercookie@gmail.com>
2022-01-07 23:22:47 -0800

    fix~: refreshing an engram page no longer causes error

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
 const session = require("express-session");
 const passport = require('passport');
 const GitHubStrategy = require('passport-github2').Strategy;
 const { Octokit } = require("@octokit/core");
 const { RequestError } = require("@octokit/request-error");
 const axios = require('axios');
-// var util = require('util');
-// var bodyParser = require('body-parser');
-// var methodOverride = require('method-override');
-// var partials = require('express-partials');
+const history = require('connect-history-api-fallback');
 
+const app = express();
+
+// configure Express
+app.use(function(req, res, next) {
+   //replace localhost:8080 to the ip address:port of your server
+   res.header("Access-Control-Allow-Origin", "http://localhost:8080"); // has to be 8080 as of this writing
+   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+   res.header('Access-Control-Allow-Headers', 'Content-Type');
+   res.header('Access-Control-Allow-Credentials', true);
+   next();
+});
+app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false, cookie: { secure: false } }));
+
+// configure Passport
 passport.serializeUser(function(user, done) {
   done(null, user);
 });
 passport.deserializeUser(function(obj, done) {
-	console.log('plz tell me this isnt called');
   done(null, obj);
 });
 passport.use(new GitHubStrategy({
@@ -34,46 +43,26 @@ passport.use(new GitHubStrategy({
 				accessToken,
 				refreshToken,
         repositoryName: '',
-        engramDataArray: [],
       };
 
-			// const userMatchIndex = users.findIndex(user => user.id === profile.id);
+			try {
+				await getEngramsDirectoryData(user);
+			} catch (error) {
+				if (error instanceof RequestError && error.status === 404 && error.request.url.endsWith('/contents/engrams')) {
+					await createEngramsDirectoryData(user);
+				} else {
+					console.error(error);
 
-			// if (userMatchIndex !== -1) {
-			// 	users[userMatchIndex].accessToken = accessToken;
-			// 	users[userMatchIndex].refreshToken = refreshToken;
-			// } else {
-			// 	users.push(user);
-			// }
-      
-			// console.log(user);
+					done(error); // or return done(null, false, { message: '...' });?
+				}
+			}
 
       return done(null, user);
     });
   }
 ));
-
-const app = express();
-
-// configure Express
-app.use(function(req, res, next) {
-   //replace localhost:8080 to the ip address:port of your server
-   res.header("Access-Control-Allow-Origin", "http://localhost:8080"); // has to be 8080 as of this writing
-   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-   res.header('Access-Control-Allow-Headers', 'Content-Type');
-   res.header('Access-Control-Allow-Credentials', true);
-   next();
-});
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
-// app.use(partials());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// app.use(methodOverride());
-app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false, cookie: { secure: false } }));
 app.use(passport.initialize());
 app.use(passport.session());
-// app.use(express.static(__dirname + '/public'));
 
 app.get('/auth/github',
   passport.authenticate('github', { scope: [ 'user:email' ], session: true }),
@@ -84,168 +73,118 @@ app.get('/auth/github',
 
 app.get('/auth/github/callback', 
   passport.authenticate('github', { failureRedirect: '/', session: true }),
-  async function(req, res) {
-    // const code = req.query.code;
-
-    console.log(req.isAuthenticated());
-		console.log(req.user);
-		console.log(req.sessionID);
-
+  function(req, res) {
     res.redirect('/engrams');
   });
 
-app.get('/engrams', async (req, res) => {
-	console.log('---------------')
-  console.log(req.isAuthenticated());
-	console.log(req.user);
-	console.log(req.sessionID);
+app.get('/engrams', ensureAuthenticated, async (req, res) => {
+	const engramTitles = [];
 
+	try {
+		const engramsDirectoryData = await getEngramsDirectoryData(req.user);
+		for (const basicEngramData of engramsDirectoryData) {
+			// const { data: engramData } = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+			// 	owner: req.user.name,
+			// 	repo: req.user.repositoryName,
+			// 	path: `engrams/${basicEngramData.name}`,
+			// });
+
+			// console.log(engramData.content)
+			engramTitles.push(basicEngramData.name.replace('.engram', ''));
+		}
+	} catch (error) {
+		console.error(error);
+	}
+
+  res.send(engramTitles);
+  });
+
+app.get('/:engramTitle', ensureAuthenticated, async (req, res) => {
+	const engramData = {};
 	const octokit = new Octokit({
 		auth: req.user.accessToken,
+	});
+
+	try {
+		const engramsDirectoryData = await getEngramsDirectoryData(req.user);
+
+		const engramFilename = engramsDirectoryData.find((basicEngramData) => basicEngramData.name.startsWith(req.params.engramTitle)).name;
+		const { data: basicEngramData } = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+			owner: req.user.name,
+			repo: req.user.repositoryName,
+			path: `engrams/${engramFilename}`,
+		});
+
+		engramData.title = engramFilename.replace('.engram', '');
+		engramData.content = basicEngramData.content;
+	} catch (error) {
+		console.error(error);
+	}
+
+	console.log(engramData);
+
+	res.send(engramData);
+});
+
+app.use(history());
+
+app.listen(3000, () => console.log('server is running on port 3000'));
+
+// Simple route middleware to ensure user is authenticated. Use this route middleware on any resource that needs to be protected.
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+		console.log('yoooo'); 
+    return next();
+  }
+	console.log('not authenticated');
+  res.redirect('/'); // I think this is fine for now?
+}
+
+async function getEngramsDirectoryData(user) {
+	const octokit = new Octokit({
+		auth: user.accessToken,
 	});
 
 	try {
 		const { data: installationData } = await octokit.request('GET /user/installations');
 		const installationId = installationData.installations[0].id;
 
-		const { data: repositoryData } = await octokit.request(`GET /user/installations/${installationId}/repositories`);
-		req.user.repositoryName = repositoryData.repositories[0].name; // get first directory only
+		const { data: repositoryData } = await octokit.request(
+			'GET /user/installations/{installation_id}/repositories',
+			{
+				installation_id: installationId,
+			}
+		);
+		user.repositoryName = repositoryData.repositories[0].name; // get first directory only
 
 		const { data: engramsDirectoryData } = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
-			owner: req.user.name,
-			repo: req.user.repositoryName,
+			owner: user.name,
+			repo: user.repositoryName,
 			path: 'engrams'
 		});
-			// make sure you have set the `Contents` permission to read & write for this part to work
-			// may need to use another method if list of engrams reach over 1k, see https://docs.github.com/en/rest/reference/repos#get-repository-content
 
-		const engramDataArray = [];
-		for (const basicEngramData of engramsDirectoryData) {
-			const { data: engramData } = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
-				owner: req.user.name,
-				repo: req.user.repositoryName,
-				path: `engrams/${basicEngramData.name}`,
-			});
-
-			// console.log(engramData.content)
-			engramDataArray.push({
-				title: basicEngramData.name.replace('.engram', ''),
-				content: engramData.content,
-			});
-		}
-		req.user.engramDataArray = engramDataArray;
+		return engramsDirectoryData;
 	} catch (error) {
-		if (err instanceof RequestError && err.status === 404 && err.request.url.endsWith('/contents/engrams')) {
-			console.log('need to create the directory');
-
-			await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', {
-				owner: req.user.name,
-				repo: req.user.repositoryName,
-				path: 'engrams/sample.engram',
-				message: 'init',
-				content: Buffer.from('* Sample').toString('base64'),
-			});
-
-			req.user.engramDataArray.push(
-				{
-					title: 'sample',
-					content: Buffer.from('* Sample').toString('base64'),
-				},
-			);
-		} else {
-			console.error(err);
-
-			done(err); // or return done(null, false, { message: '...' });?
-		}
+		throw(error);
 	}
-
-  res.send(req.user.engramDataArray);
-  });
-
-app.listen(3000, () => console.log('server is running on port 3000'));
-
-// Simple route middleware to ensure user is authenticated.
-//   Use this route middleware on any resource that needs to be protected.  If
-//   the request is authenticated (typically via a persistent login session),
-//   the request will proceed.  Otherwise, the user will be redirected to the
-//   login page.
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { 
-    console.log('authenticated wooo');
-    return next();
-  }
-  console.log('oh no');
-  res.redirect('/');
 }
 
+async function createEngramsDirectoryData(user) {
+	console.log('need to create the directory');
 
-/*
-const users = [{
-	id: 0,
-	name: 'dummy user',
-	accessToken: '0123',
-	refreshToken: '4567',
-	repositoryName: 'very original',
-	engramDataArray: [{
-		title: 'Dog',
-		content: '',
-	}],
-}];
+	const octokit = new Octokit({
+		auth: user.accessToken,
+	});
 
-const octokit = new Octokit({
-        auth: accessToken,
-      });
-
-      try {
-        const { data: installationData } = await octokit.request('GET /user/installations');
-        const installationId = installationData.installations[0].id;
-
-        const { data: repositoryData } = await octokit.request(`GET /user/installations/${installationId}/repositories`);
-        user.repositoryName = repositoryData.repositories[0].name; // get first directory only
-
-        const { data: engramsDirectoryData } = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
-          owner: user.name,
-          repo: user.repositoryName,
-          path: 'engrams'
-        });
-          // make sure you have set the `Contents` permission to read & write for this part to work
-          // may need to use another method if list of engrams reach over 1k, see https://docs.github.com/en/rest/reference/repos#get-repository-content
-
-        for (const basicEngramData of engramsDirectoryData) {
-          const { data: engramData } = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
-            owner: user.name,
-            repo: user.repositoryName,
-            path: `engrams/${basicEngramData.name}`,
-          });
-
-          // console.log(engramData.content)
-          user.engramDataArray.push({
-            title: basicEngramData.name.replace('.engram', ''),
-            content: engramData.content,
-          });
-        }
-      } catch (error) {
-        if (err instanceof RequestError && err.status === 404 && err.request.url.endsWith('/contents/engrams')) {
-          console.log('need to create the directory');
-
-          await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', {
-            owner: user.name,
-            repo: user.repositoryName,
-            path: 'engrams/sample.engram',
-            message: 'init',
-            content: Buffer.from('* Sample').toString('base64'),
-          });
-
-          user.engramDataArray.push(
-            {
-              title: 'sample',
-              content: Buffer.from('* Sample').toString('base64'),
-            },
-          );
-        } else {
-          console.error(err);
-
-          done(err); // or return done(null, false, { message: '...' });?
-        }
-      }
-*/
+	try {
+		await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', {
+			owner: user.name,
+			repo: user.repositoryName,
+			path: 'engrams/sample.engram',
+			message: 'init',
+			content: Buffer.from('* Sample').toString('base64'),
+		});
+	} catch (error) {
+		console.error(error);
+	}
+}
